7ab613f2a82a5e9c91607189215afe7e81b98d11
Kanem <kanemdeng@email.com>
2022-09-15 07:02:55 -0700

    cleanup some codes

---
+++ server/controller/uploadController.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const Books = require('../models/book')
+const fs = require('fs');
+
+
+const getRequest = async (req, res) => {
+  try {
+    res.status(201).render('create')
+  } catch (error) {
+    console.log(error)
+    res.status(404).json(error.message)
+  }
+}
+
+const postRequest = (req, res) => {
+  const product = {
+    name: req.body.name,
+    desc: req.body.desc,
+    price: req.body.price,
+    img: {
+      data: fs.readFileSync(path.join(__dirname + '../../images/' + req.file.filename)),
+      contentType: 'image/jpg'
+    }
+  }
+  Books.create(product, (error, item) => {
+    if (error) {
+      console.log(error)
+    } else {
+      res.redirect('/')
+    }
+  })
+}
+
+module.exports = { getRequest, postRequest }
