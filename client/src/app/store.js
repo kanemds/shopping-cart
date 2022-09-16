dd52c47ace51ec4589ca932c5a241fe0d051ec6a
Kanem <kanemdeng@email.com>
2022-09-16 01:36:27 -0700

    creating asyncThunk

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
-import productsReducer from '../features/productsSlice'
+import productsReducer, { getProducts } from '../features/productsSlice'
+
+
 
 const store = configureStore({
   reducer: {
     products: productsReducer
   }
 })
+
+store.dispatch(getProducts)
 
 export default store
