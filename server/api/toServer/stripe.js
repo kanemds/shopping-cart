e4ce218d55796a7ad1ddae9488ac2732c9068d9c
Kanem <kanemdeng@email.com>
2022-09-21 13:21:01 -0700

    carts items are now able to show in stripe

--- server/api/toServer/stripe.js
+++ server/api/toServer/stripe.js
@@ -3,22 +3,34 @@ const router = express.Router();
 const Stripe = require('stripe')
 require('dotenv').config()
 const stripe = Stripe(process.env.STRIPE_KEY)
+const { Blob } = require('buffer')
 
 
 router.post('/create-checkout-session', async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        price_data: {
-          currency: 'usd',
-          product_data: {
-            name: 'T-shirt',
-          },
-          unit_amount: 2000,
+
+  const line_items = req.body.cartItems.map(product => {
+    // console.log(product)
+    // const blob = new Blob([Int8Array.from(product.img.data.data)], { type: product.img.contentType })
+    // const image = window.URL.createObjectURL(blob);
+    // console.log(image)
+    return {
+      price_data: {
+        currency: 'cad',
+        product_data: {
+          name: product.name,
+          description: product.desc,
+          metadata: {
+            id: product._id
+          }
         },
-        quantity: 1,
+        unit_amount: product.price * 100,
       },
-    ],
+      quantity: product.cartQuantity
+    }
+  })
+
+  const session = await stripe.checkout.sessions.create({
+    line_items,
     mode: 'payment',
     success_url: `${process.env.CLIENT_URL}/checkout-success`,
     cancel_url: `${process.env.CLIENT_URL}/cart`,
