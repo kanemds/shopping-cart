eefc36af25c1f40179f509841bfc53993688728a
Kanem <kanemdeng@email.com>
2022-09-23 15:57:18 -0700

    connect with firebase

--- client/src/components/admin/CreateProduct.js
+++ client/src/components/admin/CreateProduct.js
@@ -1,7 +1,39 @@
-import React from 'react'
-import { Box, Button, Typography, TextField, FormControl, Input, InputLabel, InputAdornment, OutlinedInput, CardActionArea, CardMedia, Stack } from '@mui/material'
+import React, { useState, useEffect } from 'react'
+import { Box, Button, Typography, TextField, FormControl, InputLabel, InputAdornment, OutlinedInput, CardMedia, Card } from '@mui/material'
+
+import storage from "../../firebase"
+import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage"
 
 const CreateProduct = () => {
+
+  // store the image in state then upload to firebase
+  const [image, setImage] = useState(null)
+  const [imageList, setImageList] = useState([])
+  const imageListRef = ref(storage, 'producs/')
+
+  const handleUpload = (e) => {
+    if (setImage == null) return
+    // path
+    const fileName = new Date().getTime() + image.name
+    const imageRef = ref(storage, `products/${fileName}`)
+    uploadBytes(imageRef, image).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then(url => {
+        setImageList(url)
+      })
+    })
+  }
+
+  useEffect(() => {
+    listAll(imageListRef).then(res => {
+      res.items.forEach(item => {
+        getDownloadURL(item).then(url => {
+          setImageList(url)
+        }
+        )
+      })
+    })
+  })
+
   return (
     <>
 
@@ -20,24 +52,29 @@ const CreateProduct = () => {
                 label="Amount"
               />
             </FormControl>
-            <Stack sx={{ m: 1 }}>
-              <Button variant="contained" component="label">
-                Upload Product
-                <input hidden accept="image/*" multiple type="file" />
-              </Button>
-            </Stack>
+            <FormControl sx={{ m: 1 }}>
+              <OutlinedInput
+                type='file'
+                onChange={e => setImage(e.target.files[0])}
+              />
+              <Button onClick={handleUpload}>Upload</Button>
+            </FormControl>
+
           </Box>
-          <Box sx={{ maxWidth: 600, width: '100%', m: 2 }}>
-            <CardActionArea>
+          <Box sx={{ display: 'flex', ml: 20 }}>
+            <Card >
               <CardMedia
                 component="img"
-                height="400"
-                image="abc"
+                height="500"
+                width="400"
+                image={imageList}
                 alt="Product Image"
               />
-            </CardActionArea>
+
+            </Card>
           </Box>
         </Box>
+        <Button>Create</Button>
       </Box>
     </>
   )
