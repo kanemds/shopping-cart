a88ccd939c57018cbea08bf036745545c7193e93
Kanem <kanemdeng@email.com>
2022-09-26 23:54:02 -0700

    able to print out previous month

--- client/src/components/admin/Summary.js
+++ client/src/components/admin/Summary.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import Grid from '@mui/material/Unstable_Grid2'
-import { styled } from '@mui/material/styles'
-import { Box, Paper, Typography, Card } from '@mui/material'
+
+import { Box, Paper, Typography } from '@mui/material'
 import GroupsIcon from '@mui/icons-material/Groups';
 import LocalMallIcon from '@mui/icons-material/LocalMall';
 import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
@@ -9,58 +8,52 @@ import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
 const Summary = () => {
 
 
-
-
   return (
-    <>
-      <Box >
+    <Box >
+      <Paper sx={{ display: 'flex', flexDirection: 'column', backgroundColor: "rgba(1, 40, 106, 0.8)", color: "white" }}>
+        <Box sx={{ m: 2 }} align='left'>
+          <Typography variant='h4'>Overview</Typography>
+          <Typography>Report from the previous month</Typography>
+        </Box>
 
-        <Paper sx={{ display: 'flex', flexDirection: 'column', backgroundColor: "rgba(1, 40, 106, 0.8)", color: "white" }}>
-          <Box sx={{ m: 2 }} align='left'>
-            <Typography variant='h4'>Overview</Typography>
-            <Typography>Report from the previous month</Typography>
+        <Box sx={{ display: 'flex', justifyContent: 'space-around', mt: 2, mb: 2 }}>
+
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Box sx={{ height: '100%', display: 'flex', alignItems: 'center', backgroundColor: "rgba(35, 177, 3, 0.3)", borderRadius: .7, p: 1 }}>
+              <GroupsIcon fontSize='large' />
+            </Box>
+            <Box sx={{ ml: 1 }}>
+              <Typography variant='h5'>50</Typography>
+              <Typography>Users</Typography>
+            </Box>
+            <Typography sx={{ ml: 1 }}>50%</Typography>
           </Box>
 
-          <Box sx={{ display: 'flex', justifyContent: 'space-around', mt: 2, mb: 2 }}>
-
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-              <Box sx={{ backgroundColor: "rgba(35, 177, 3, 0.3)", borderRadius: .7 }}>
-                <GroupsIcon fontSize='large' />
-              </Box>
-              <Box>
-                <Typography>50</Typography>
-                <Typography>Users</Typography>
-              </Box>
-              <Typography>50%</Typography>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Box sx={{ height: '100%', display: 'flex', alignItems: 'center', backgroundColor: "rgba(244, 255, 0, 0.3)", borderRadius: .7, p: 1 }}>
+              <LocalMallIcon fontSize='large' />
             </Box>
-
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-              <Box sx={{ backgroundColor: "rgba(244, 255, 0, 0.3)", borderRadius: .7 }}>
-                <LocalMallIcon fontSize='large' />
-              </Box>
-              <Box>
-                <Typography>50</Typography>
-                <Typography>Orders</Typography>
-              </Box>
-              <Typography>50%</Typography>
+            <Box sx={{ ml: 1 }}>
+              <Typography variant='h5'>50</Typography>
+              <Typography>Orders</Typography>
             </Box>
-
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-              <Box sx={{ backgroundColor: "rgba(8, 212, 226, 0.3)", borderRadius: .7 }}>
-                <MonetizationOnIcon fontSize='large' />
-              </Box>
-              <Box>
-                <Typography>50</Typography>
-                <Typography>Users</Typography>
-              </Box>
-              <Typography>50%</Typography>
-            </Box>
+            <Typography sx={{ ml: 1 }}>50%</Typography>
           </Box>
-        </Paper>
 
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Box sx={{ height: '100%', display: 'flex', alignItems: 'center', backgroundColor: "rgba(8, 212, 226, 0.3)", borderRadius: .7, p: 1 }}>
+              <MonetizationOnIcon fontSize='large' />
+            </Box>
+            <Box sx={{ ml: 1 }}>
+              <Typography variant='h5'>50</Typography>
+              <Typography>Earning</Typography>
+            </Box>
+            <Typography sx={{ ml: 1 }}>50%</Typography>
+          </Box>
+        </Box>
+      </Paper>
+    </Box>
 
-      </Box>
-    </>
 
   )
 }
