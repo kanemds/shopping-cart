ea321fad73d4a52b39cd2665c5353485595c7363
Kanem <kanemdeng@email.com>
2022-10-02 01:22:55 -0700

    created get request route for single order

--- client/src/components/details/Order.js
+++ client/src/components/details/Order.js
@@ -1,8 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Box, Paper, Typography } from '@mui/material'
+import axios from 'axios'
+import { api, setHeaders } from '../../features/api'
+import { useParams } from 'react-router-dom'
 
 const Order = () => {
+
+  const { id } = useParams()
+
+  const [currentOrder, setCurrentOrder] = useState({})
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const getOrder = async () => {
+      try {
+        setLoading(true)
+        const response = await axios.get(`${api}/order/find/${id}`, setHeaders())
+
+        setCurrentOrder(response.data)
+        setLoading(false)
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
+    getOrder()
+  }, [id])
+
+  console.log(currentOrder)
+
+
   return (
-    <div>Order</div>
+    <>
+      <Paper>
+        <Box>
+          <Typography>Order Details</Typography>
+
+          <Typography>
+            Delivery Status: {
+              currentOrder.delivery_status === "pending" ? <Typography>Pending</Typography> :
+                currentOrder.delivery_status === "dispatched" ? <Typography>Dispatched</Typography> :
+                  currentOrder.delivery_status === "delivered" ? <Typography>Delivered</Typography> :
+                    <Typography>System Error</Typography>
+            }
+          </Typography>
+        </Box>
+        <Box>
+          <Typography>Products</Typography>
+        </Box>
+        <Box>
+          <Typography>Total</Typography>
+        </Box>
+        <Box>
+          <Typography>Shipping Details</Typography>
+        </Box>
+      </Paper>
+
+
+
+    </>
   )
 }
 
--- server/api/toClient/order.js
+++ server/api/toClient/order.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const { getRequest, getEarningRequest, getWeeklyRequest, getLastRequest, putRequest } = require('../../controller/toClient/orderController')
+const { getRequest, getEarningRequest, getWeeklyRequest, getLastRequest, putRequest, getOneRequest } = require('../../controller/toClient/orderController')
 const { auth, isUser, isAdmin } = require('../middleware/auth')
 
 router.get('/earning', isAdmin, getEarningRequest)
 router.get('/weekly-sales', isAdmin, getWeeklyRequest)
 router.get('/overview', isAdmin, getRequest)
 router.get('/translists', isAdmin, getLastRequest)
+router.get('/find/:id', auth, getOneRequest)
 router.put('/:id', isAdmin, putRequest)
 
 module.exports = router
